280d660bd74d8c2d6704be6dd5098ce8e85d290e
Rheola <okunewa@yandex.ru>
2016-09-28 11:20:26 +0300

    Сгенерированное приложение.

---
+++ app/public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+|| define('APPLICATION_PATH', __DIR__ . '/../application');
+
+// Define application environment
+defined('APPLICATION_ENV')
+|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, [
+    APPLICATION_PATH . '/../library',
+    get_include_path(),
+]));
+
+/** Zend_Application */
+require_once __DIR__ . '/../library/Zend/Application.php';
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap()
+    ->run();
