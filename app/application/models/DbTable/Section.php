bf03d546facf01e902edf99c2921e76de520525e
Rheola <okunewa@yandex.ru>
2016-09-28 22:49:19 +0300

    Модель Section

---
+++ app/application/models/DbTable/Section.php
@@ -0,0 +1,35 @@
+<?php
+
+class Application_Model_Section extends Zend_Db_Table_Abstract
+{
+
+
+    protected $_name = 'section';
+
+
+    public $name;
+    public $_id;
+
+ /*   public function __construct(array $options = null)
+    {
+        if (is_array($options)) {
+            $this->setOptions($options);
+        }
+    }*/
+
+
+    public function setOptions(array $options)
+    {
+        $methods = get_class_methods($this);
+        foreach ($options as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (in_array($method, $methods)) {
+                $this->$method($value);
+            }
+        }
+        return $this;
+    }
+
+
+}
+
