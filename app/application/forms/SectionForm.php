6db3c65ffff45dad5e1cf6ddeae6747343257750
Rheola <okunewa@yandex.ru>
2016-09-29 22:22:31 +0300

    Кнопка формы, валидация,  CSRF.

--- app/application/forms/SectionForm.php
+++ app/application/forms/SectionForm.php
@@ -9,15 +9,31 @@ class Application_Form_SectionForm extends Zend_Form
         $this->setMethod('post');
 
         // Add an email element
-        $this->addElement('text', 'name', [
-            'label' => 'Название рубрики',
-            'required' => true,
-            'filters' => ['StringTrim'],
-
-            'validators' => [
-                ['validator' => 'StringLength', 'options' => [0, 255]]
+        $this->addElement(
+            'text', 'name', [
+                'label'      => 'Название',
+                'required'   => true,
+                'filters'    => ['StringTrim'],
+                'validators' => [
+                    ['validator' => 'StringLength', 'options' => [0, 255]]
+                ]
             ]
-        ]);
+        );
+
+        // Add the submit button
+        $this->addElement(
+            'submit', 'submit', [
+                'ignore' => true,
+                'label'  => 'Добавить',
+            ]
+        );
+
+        // And finally add some CSRF protection
+        $this->addElement(
+            'hash', 'csrf', [
+                'ignore' => true,
+            ]
+        );
 
     }
 }
